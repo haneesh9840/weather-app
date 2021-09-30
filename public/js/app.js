45b7d9f789a92ef4c10c46c26f1959c8829110f7
haneesh9840 <61122036+haneesh9840@users.noreply.github.com>
2021-09-30 11:57:28 +0530

    updated

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ query.addEventListener('submit', (e) => {
     msg2.textContent = ""
     msg1.textContent = "Loading..!"
 
-    fetch('http://localhost:3000/weather?address=' + search.value).then((response) => {
+    fetch('/weather?address=' + search.value).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 msg1.textContent = data.error
